a3948378e142ce5f4ba39d2e6b6d4e490e5afa17
Berke Güleryüz <berkeguleryuz@gmail.com>
2025-04-07 13:40:25 +0200

    Initial commit from Create Next App

---
+++ app/next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
